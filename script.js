89e1efd7456a4e6f7441d1514c05445d8d59aec4
Alex Zhu <alexazhu@amazon.com>
2017-04-12 18:12:16 -0700

    Basic setup

---
+++ script.js
@@ -0,0 +1,15 @@
+angular.module('Scoreboard', ['ngMaterial'])
+
+.controller('ScoreboardController', ['$scope', function($scope) {
+    $scope.scores = [0, 0];
+
+    $scope.increment = function(index) {
+        $scope.scores[index]++;
+    };
+
+    $scope.reset = function() {
+        for (var i = 0; i < $scope.scores.length; i++) {
+            $scope.scores[i] = 0;
+        }
+    };
+}]);
